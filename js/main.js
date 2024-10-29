e598783f5aa0ff5d93ef984a814c0c70a77e0303
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2024-10-29 21:33:04 +0100

    Ajouter la base de code brief_4

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function add(){
+    let titre=document.getElementById('titre')
+    let descr=document.getElementById('description')
+    let etat=document.getElementById('etat')
+    let date=document.getElementById('date')
+    window.alert("Titre"+titre.value+"description: "+descr.value+"etat: "+etat.value+"date: "+date.value )
+
+}
